b6734739466f82bd16ae00a2cc4e50344a118d03
Daniel Louw <daniel.louw@platform45.com>
2021-10-06 08:18:02 +0200

    Added sample credentials file

---
+++ nanoview-wifi-bridge/src/credentials.sample.h
@@ -0,0 +1,4 @@
+const char* WIFI_SSID = "your wifi ssid";
+const char* WIFI_PASSWORD = "your wifi password";
+
+const char* influx_server_string = "http://url.to.influx.server:8086/write?db=db_name.power&u=user&p=password";
