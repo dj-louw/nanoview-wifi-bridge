04c6706beaae9253283c8066f7de872087b03578
Daniel Louw <daniel.louw@platform45.com>
2021-11-07 12:28:46 +0200

    Added update link to html home page to make updates eaier.

--- src/OTA.h
+++ src/OTA.h
@@ -4,7 +4,66 @@
 #include <AsyncElegantOTA.h>
 #include <TelnetStream.h>
 
+#include "html.h"
+
+
+bool ledState = 0;
+const int ledPin = 2;
+
+// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void notifyClients() {
+  ws.textAll(String(ledState));
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle") == 0) {
+      ledState = !ledState;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (ledState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  return String();
+}
 
 void setupOTA(const char* wifissid, const char* wifipassword)
 {
@@ -23,12 +82,22 @@ void setupOTA(const char* wifissid, const char* wifipassword)
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 
+    initWebSocket();
+
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "Hi! I am ESP32.");
+    request->send_P(200, "text/html", index_html, processor);
     });
 
     AsyncElegantOTA.begin(&server);    // Start ElegantOTA
     TelnetStream.begin();
     server.begin();
   Serial.println("HTTP server started");
+
+};
+
+void OTALoopFunctions()
+{
+  AsyncElegantOTA.loop();
+  ws.cleanupClients();
+  digitalWrite(ledPin, ledState);
 };
