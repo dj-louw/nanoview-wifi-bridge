6f214286c21ae93db05d5396d801e818d07df7d7
Daniel Louw <daniel.louw@platform45.com>
2022-03-25 22:36:08 +0200

    Cleaned up nanoview.h

--- src/nanoview.h
+++ src/nanoview.h
@@ -15,32 +15,15 @@ void print_live_power(struct nv_live_power *nvlp) {
 
     String ipaddr = WiFi.localIP().toString();
 
+    // todo move this to the influx file, so that we keep everything nice and compartmentalized.
     String payload_power = "Nanoview_Power_Measurement,source_ip="+ ipaddr +",type=live_power CH00="+String(nvlp->live_power_w[0])+",CH01="+String(nvlp->live_power_w[1])+",CH02="+String(nvlp->live_power_w[2])+",CH03="+String(nvlp->live_power_w[3])+",CH04="+String(nvlp->live_power_w[4])+",CH05="+String(nvlp->live_power_w[5])+",CH06="+String(nvlp->live_power_w[6])+",CH07="+String(nvlp->live_power_w[7])+",CH08="+String(nvlp->live_power_w[8])+",CH09="+String(nvlp->live_power_w[9])+",CH10="+String(nvlp->live_power_w[10])+",CH11="+String(nvlp->live_power_w[11])+",CH12="+String(nvlp->live_power_w[12])+",CH13="+String(nvlp->live_power_w[13])+",CH14="+String(nvlp->live_power_w[14])+",CH15="+String(nvlp->live_power_w[15]) + "\n";
     String payload_volts = "Nanoview_Power_Measurement,source_ip="+ ipaddr +",type=voltage V=" + String(nvlp->mains_voltage_v) + "\n";
-    // TelnetStream.println(payload_volts);
-    // TelnetStream.println(payload_power);
 
     int res = sendToInflux(payload_volts);
     TelnetStream.println(res);
 
     res = sendToInflux(payload_power);
     TelnetStream.println(res);
-
-
-    // String payload = "Volts: " + String(nvlp->mains_voltage_v) + "V |";
-    
-    // for (int i = 0; i < 16; i++) {
-    //     payload += String(nvlp->live_power_w[i]) + "W |";
-    // }
-
-    // TelnetStream.println(payload);
-
-    // TelnetStream.printf("%-20s", "Live Energy: ");
-    // TelnetStream.printf("%5dV", nvlp->mains_voltage_v);
-    // for (int i = 0; i < 16; i++) {
-    //     TelnetStream.printf("%5dW", nvlp->live_power_w[i]);
-    // }
-    // TelnetStream.printf("\n");
 }
 
 void print_accumulated_energy(struct nv_accumulated_energy *nvae) {
