ef14761b7726f0a508971db23eb96fffe955e168
Daniel Louw <daniel.louw@platform45.com>
2022-03-25 22:33:44 +0200

    Cleaned up influx.h

--- src/influx.h
+++ src/influx.h
@@ -1,43 +1,8 @@
-
-int counter = 0;
-
-void setupInflux()
-{
-    counter = 0;
-}
-
-void loopInflux()
-{
-    HTTPClient http;
-    http.begin(influx_server_string);
-    http.addHeader("Content-Type", "application/binary");
-
-
-    String payload = "TestMeasurement,tagKey=testTag counter=" + String(counter*(counter + 1)) + "\n";
-
-
-
-    // for(int i=0; i<bufferIndex; i++)
-    // {
-        // pos = sprintf(pos,"moisture level=%d,threshold=%d %d000000000\n",
-        //                measBuffer[i],thresholdBuffer[i],timestampBuffer[i]);
-    // }    
-
-    //sleep(1);
-
-    http.POST(payload);
-    http.end();
-}
-
 int sendToInflux(String payload)
 {
     HTTPClient http;
     http.begin(influx_server_string);
     http.addHeader("Content-Type", "application/binary");
-
-
-    //sleep(1);
-
     int res = http.POST(payload);
     http.end();
     return res;
